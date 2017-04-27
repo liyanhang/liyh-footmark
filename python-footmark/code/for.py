bb97e229d010f193d12e7d6138a69ef34320bcd3
liyanhang <liyh@gohighsec.com>
2017-04-27 14:38:46 +0800

    Some codes of this week

---
+++ python-footmark/code/for.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# Filename: for.py
+
+for i in range(1,15):
+    print i
+else:
+    print 'xunhuan wanl'
---
+++ python-footmark/code/while.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# Filename: while.py
+
+number = 23
+running = True
+
+while running:
+    guess = int(raw_input('shuru canshu:'))
+
+    if guess==number:
+        print 'dkddkd'
+    elif guess<number:
+        print 'dkdjdkd'
+    else:
+        running = False
+        print 'dkddkfj'
+
+else:
+    print 'dkdkdkdkdkdk'
+
+print 'DDDDDDD'
