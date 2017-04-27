c7af44605be25534aefdbedf61ae4c4efedc555e
liyanhang <liyh@gohighsec.com>
2017-04-27 14:38:46 +0800

    some codes of this week

---
+++ python-footmark/code/if.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# Filename: if.py
+
+number=23
+guess=int(raw_input('Enter an integer:'))
+
+if guess==number:
+    print'woaini'
+elif guess<number:
+    print'niaiwo'
+    # do whatever you want 
+else:
+    print'no, it is ugly'
+
+print'DDDD'
+# jieshaoyixia
+
+if False:
+    print 'haobuhao'
---
+++ python-footmark/code/var.py
@@ -0,0 +1,9 @@
+# Filename:var.py
+i=5
+print i
+i=i+1
+print i
+
+s='''This a beauty girl,
+     i love her.'''
+print s 
